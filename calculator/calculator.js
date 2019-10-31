f5e96c80412dd9bec15cd32c983e099234b7bc3a
Robin Andrea <robinandrea_2@yahoo.com>
2019-10-31 16:35:35 -0700

    complete the calculator assignment

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,44 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (array) {
+	let sum = 0;
+	array.forEach( function(item) {
+		sum += item;
+	});
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let result = 1;
+	for (let i of array){
+		result *= i;
+	}
+	return result;
 }
 
-function power() {
-	
+function power(num1, num2) {
+	return Math.pow(num1, num2);
 }
 
-function factorial() {
+function factorial(num) {
+	let result = num;
+
+	if (num == 0 || num == 1) {
+		return 1;
+	} else {
+		while (num > 1) {
+			num--;
+			result *= num;
+		}
+	}
 	
+	return result;
 }
 
 module.exports = {
