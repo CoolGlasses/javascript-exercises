f060ed08341e9fb00a87803a668736111e0a2832
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-01 09:32:56 -0700

    completed fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let fibArray = [0, 1];
+
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    if (num == 1) {
+        return fibArray[1];
+    }
+
+    for (let i = 1; i <= num; i++) {
+        let nextNum = fibArray[i - 1] + fibArray[i];
+        fibArray.push(nextNum);
+    }
+
+    console.log(fibArray);
+    return fibArray[num];
+
 
 }
 
