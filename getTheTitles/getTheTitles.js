268130c522434dc3f5cf7695839989d2e933cfa2
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-01 09:39:57 -0700

    completed the getTheTitles exercise

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(arrayOfHashes) {
+    let finalArray = [];
 
+    for (let i = 0; i < arrayOfHashes.length; i++) {
+        let hash = arrayOfHashes[i];
+        finalArray.push(hash.title);
+    }
+
+    return finalArray;
 }
 
 module.exports = getTheTitles;
