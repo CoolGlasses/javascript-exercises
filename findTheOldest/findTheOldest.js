c70466fcfaec4af17502f187db4ce9e0d89da66f
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-01 09:51:08 -0700

    completed findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-let findTheOldest = function() {
+let findTheOldest = function(arrayOfHashes) {
+    let personOfInterest;
+    let oldestAge = 0;
+
+    for (let i = 0; i < arrayOfHashes.length; i++) {
+        let hash = arrayOfHashes[i];
+        let age = 0;
+
+        if (hash.yearOfDeath == undefined) {
+            let currentDate = new Date();
+            age = currentDate.getFullYear() - hash.yearOfBirth;
+        } else {
+            age = hash.yearOfDeath - hash.yearOfBirth;
+        }
+
+        if (oldestAge < age) {
+            oldestAge = age;
+            personOfInterest = hash;
+        }
+    }
+
+    return personOfInterest;
 
 }
 
